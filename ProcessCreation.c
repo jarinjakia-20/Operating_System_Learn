4064283b5b0666f210144ca8911aa8d592eddd05
jarinjakia-20 <168300343+jarinjakia-20@users.noreply.github.com>
2025-09-05 22:48:22 +0600

    first commit

---
+++ ProcessCreation.c
@@ -0,0 +1,21 @@
+// Process control
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+    pid_t pid = fork();
+
+    if (pid == 0) { // child
+        printf("Child PID: %d\n", getpid());
+        execlp("gedit", "gedit", NULL);  // replace process with gedit
+    } else if (pid > 0) { // parent
+        wait(NULL);  // wait for child
+        printf("Parent PID: %d\n", getpid());
+    } else {
+        perror("fork failed");
+    }
+
+    return 0;
+}
